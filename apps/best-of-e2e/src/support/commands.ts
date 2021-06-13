5e8b30ee76e943f1884137b33eeac81e4dab4a97
Jonathan Pollock <milan@darkrush.photo>
2021-06-13 16:13:44 -0400

    Remove auto-generated cypress display of password

--- apps/best-of-e2e/src/support/commands.ts
+++ apps/best-of-e2e/src/support/commands.ts
@@ -12,13 +12,13 @@
 declare namespace Cypress {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   interface Chainable<Subject> {
-    login(email: string, password: string): void;
+    login(email: string): void;
   }
 }
 //
 // -- This is a parent command --
-Cypress.Commands.add('login', (email, password) => {
-  console.log('Custom command example: Login', email, password);
+Cypress.Commands.add('login', (email) => {
+  console.log('Custom command example: Login', email);
 });
 //
 // -- This is a child command --
