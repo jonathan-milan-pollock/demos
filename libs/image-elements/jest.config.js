912d69482177b6e72bf2371359dc3666f9c9bb61
Jonathan Pollock <milan@darkrush.photo>
2021-09-27 13:17:57 -0400

    Adding test all for pull request and commit

--- libs/image-elements/jest.config.js
+++ libs/image-elements/jest.config.js
@@ -1,5 +1,13 @@
 module.exports = {
   displayName: 'image-elements',
+  testRunner: 'jasmine2',
   preset: '@stencil/core/testing',
+  globals: {
+    'ts-jest': { tsconfig: '<rootDir>/tsconfig.spec.json' },
+  },
+  transform: {
+    '^.+\\.[tj]sx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../coverage/libs/image-elements',
 };
